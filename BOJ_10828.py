48420604d860124a9941f89b00529539181b9ff2
Sumin Kim <ksumin0730@naver.com>
2022-01-06 11:55:01 +0900

    BOJ_10828

---
+++ BOJ_10828.py
@@ -0,0 +1,31 @@
+#스택
+
+import sys
+
+stack = []
+
+N = int(input())
+for _ in range (N):
+    input = sys.stdin.readline()
+    do = str(input)
+    if do.startswith("push"):
+        num = do.split()[-1]
+        stack.append(num)
+    elif do.startswith("pop"):
+        if len(stack) == 0:
+        print(-1)
+        else :
+        print(stack.pop())
+    elif do.startswith("size"):
+        print(len(stack))
+    elif do.startswith("empty"):
+        if len(stack)==0 : 
+            print (1)
+        else:
+            print(0)
+    elif do.startswith("top"):
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
+    
