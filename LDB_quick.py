35bd4f6589962dc7ac59050be4c9511b0f522ccc
Sumin Kim <ksumin0730@naver.com>
2022-01-09 16:04:27 +0900

    sort

--- LDB_quick.py
+++ LDB_quick.py
@@ -5,6 +5,7 @@ array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
 def quick_sort(array, start, end):
     if start >= end: #원소가 1개인 경우 종료
         return
+        
     pivot = start #피벗은 첫번째원소
     left = start + 1
     right = end
