8b5f8bab1169a6960487134afba0348bb365023f
Sumin Kim <ksumin0730@naver.com>
2022-01-09 16:25:51 +0900

    sort

---
+++ BOJ_1181.py
@@ -0,0 +1,16 @@
+#단어정렬
+
+import sys
+
+n = int(input())
+lists = []
+
+for i in range(n):
+    lists.append(sys.stdin.readline().strip())
+sets = set(lists)
+lists = list(sets)
+lists.sort()
+lists.sort(key = len)
+
+for i in lists:
+    print(i)
