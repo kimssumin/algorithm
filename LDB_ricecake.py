806dc4b23ff3a104d139e8027377cbbbc69018e9
Sumin Kim <ksumin0730@naver.com>
2022-01-11 17:06:54 +0900

    search

--- LDB_ricecake.py
+++ LDB_ricecake.py
@@ -2,7 +2,7 @@
 #떡볶이 떡 만들기
 #parametric search - 원하는 조건을 만족하는 가장 알맞은 값을 찾는 문제 -> 이진탐색을 이용하여 해결
 
-n, m = list(map(int, input().split( )))
+n, m = list(map(int, input().split()))
 array = list(map(int, input().split()))
 
 start = 0
