a6daa9eac80c7d49b128060e3e4a5b344c28081d
Sumin Kim <ksumin0730@naver.com>
2022-01-19 14:08:34 +0900

    DP

---
+++ BOJ_9095.py
@@ -0,0 +1,16 @@
+#DP
+
+T = int(input())
+def sums(n):
+    if n == 1:
+        return(1)
+    elif n == 2:
+        return(2)
+    elif n == 3:
+        return(4)
+    else:
+        return sums(n-1) + sums(n-2) + sums(n-3) #규칙을 찾아내는게 중요!
+        
+for _ in range(T):
+    n = int(input())
+    print(sums(n))
