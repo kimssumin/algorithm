08a721ff91a7c984f9597ea90a9c494ffd5d4c87
Sumin Kim <ksumin0730@naver.com>
2022-01-29 18:05:22 +0900

    brute

---
+++ BOJ_1107.py
@@ -0,0 +1,20 @@
+#리모컨
+#brute force
+
+n = int(input())
+m = int(input())
+cnt = abs(100 - n) # +, -로 이동 가능한 경우
+if m != 0 :
+    exc = set(input().split())
+else:
+    exc = set()
+
+
+for num in range(1000001):
+    for i in str(num):
+        if i in exc: #번호를 눌러서 이동 못하는 경우
+            break
+    else:
+        cnt = min(cnt, len(str(num)) + abs(num - n))
+
+print(cnt)
