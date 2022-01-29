1c585f58e50751bdc8c9b5790b74948c6560f3f4
Sumin Kim <ksumin0730@naver.com>
2022-01-29 18:40:10 +0900

    brute

---
+++ BOJ_1476.py
@@ -0,0 +1,14 @@
+#날짜 계산
+#brute force
+# 
+
+e, s, m = map(int, input().split())
+year = 1
+
+while (1):
+    if (year - e) % 15 ==0 and (year - s) % 28 ==0 and (year - m) % 19==0:
+        print(year)
+        break
+    year += 1
+
+    
