020cfa77ac4b2c7edc2341717b3d0148e58576da
Sumin Kim <ksumin0730@naver.com>
2022-02-05 11:18:44 +0900

    brute

---
+++ BOJ_9663.py
@@ -0,0 +1,35 @@
+#N_Queen
+#brute force
+#dfs
+
+import sys
+sys.setrecursionlimit(100000)
+
+n = int(input())
+result = 0
+col = [0]* n
+visited = [False for _ in range(n)]
+
+def check(x):
+    for i in range(x):
+        if col[x] == col[i] or abs(col[i]-col[x]) == x-i:
+            return False
+    return True
+
+def dfs(x):
+    global result
+    if x== n:
+        result += 1
+        return
+    else:
+        for i in range(n):
+            if visited[i]:
+                continue
+            col[x] = i #(x, i)에 퀸올리기
+            if check(x):
+                visited[i] = True
+                dfs(x+1) #다음행으로 
+                visited[i] = False
+
+dfs(0)
+print(result)
