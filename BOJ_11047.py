4007bf8d2aca26490b9b1dad38dd7646b8beb119
Sumin Kim <ksumin0730@naver.com>
2022-02-08 12:54:13 +0900

    greedy

--- BOJ_11047.py
+++ BOJ_11047.py
@@ -10,4 +10,11 @@ A_l = []
 
 for _ in range(n):
     A_l.append(int(input().strip()))
-  
+
+cnt = 0
+
+for i in reversed(range(len(A_l))):
+    cnt += k // A_l[i]
+    k = k % A_l[i]
+
+print(cnt)
