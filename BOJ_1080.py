3a06f456aad982baf8803a5284e36a348d3579f0
Sumin Kim <ksumin0730@naver.com>
2022-02-09 12:26:11 +0900

    greedy

---
+++ BOJ_1080.py
@@ -0,0 +1,17 @@
+
+#행렬
+#greedy
+
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+a = []
+b = []
+
+for _ in range(n) :
+    a.append(list(input().strip()))
+for _ in range(n):
+    b.append(list(input().strip()))
+
