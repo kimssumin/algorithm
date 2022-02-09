50800a4228549258703c66eabf8793178835bda4
Sumin Kim <ksumin0730@naver.com>
2022-02-09 12:30:44 +0900

    greedy

--- BOJ_1080.py
+++ BOJ_1080.py
@@ -9,9 +9,31 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 a = []
 b = []
+cnt = 0
 
 for _ in range(n) :
     a.append(list(input().strip()))
 for _ in range(n):
     b.append(list(input().strip()))
 
+def reverse(x,y):
+    for i in range(x, x+3):
+        for j in range(y, y+3):
+            a[i][j] = 1 - a[i][j]
+
+def  check():
+    for i in range(n):
+        for j in range(m):
+            if a[i][j] != b[i][j]:
+                return False
+    return True
+
+for i in range(n-2):
+    for j in range(m-2):
+        if a[i][j] != b[i][j]:
+            reverse(i, j)
+            cnt += 1
+if check():
+    print(cnt)
+else:
+    print(-1)
