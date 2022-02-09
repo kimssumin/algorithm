7a78a11e8e6e2a7a198f2ef792ba58c1737fb7cf
Sumin Kim <ksumin0730@naver.com>
2022-02-09 13:41:55 +0900

    greedy

---
+++ BOJ_1541.py
@@ -0,0 +1,20 @@
+#잃어버린 괄호
+#greedy
+
+import sys
+
+input = sys.stdin.readline 
+
+cal = input().split('-')
+num = []
+for i in cal:
+    cnt = 0
+    plus = i.split('+')
+    for j in plus:
+        cnt += int(j)
+    num.append(cnt)
+n = num[0]
+for i in range(1, len(num)):
+    n -= num[i]
+
+print(n)
