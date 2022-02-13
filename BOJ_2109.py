3238269116ac6ce9babd17f452eded0f963cb629
Sumin Kim <ksumin0730@naver.com>
2022-02-13 12:24:35 +0900

    greedy

--- BOJ_2109.py
+++ BOJ_2109.py
@@ -8,8 +8,8 @@ input = sys.stdin.readline
 n = int(input())
 lec = []
 for _ in range(n):
-    lec.sppend(list(map(int, input().split())))
-lec.sorted(lec, key = lambda x:x[1]) #데드라인 기준 오름차순
+    lec.append(list(map(int, input().split())))
+lec.sort(key = lambda x:x[1]) #데드라인 기준 오름차순
 result = []
 for i in lec:
     heapq.heappush(result, i[0])
