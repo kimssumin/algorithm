cdb2c7e667b63807d36dfaa2bab134f5ea0f93b8
Sumin Kim <ksumin0730@naver.com>
2022-02-14 09:58:16 +0900

    search

---
+++ BOJ_10815.py
@@ -0,0 +1,28 @@
+#숫자카드
+#binary search
+
+import sys
+
+n = int(input())
+cards = list(map(int, sys.stdin.readline().split()))
+m = int(input())
+check = list(map(int, sys.stdin.readline().split()))
+
+cards.sort()
+
+def binary(array, target, start, end):
+    while start <= end:
+        mid = (start + end) //2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+for i in range(m):
+    if binary(cards, check[i], 0, n-1) is not None:
+        print(1, end = '')
+    else:
+        print(0, end = '')
