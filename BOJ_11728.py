0f423ae856e1887c1f6ac5f1b8ace3969e5ce323
Sumin Kim <ksumin0730@naver.com>
2022-02-15 22:43:13 +0900

    sort

--- BOJ_11728.py
+++ BOJ_11728.py
@@ -1,3 +1,12 @@
 #배열 합치기
 #search
 
+import sys
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+res = sorted(a+b)
+print(" ".join(map(str, res)))
