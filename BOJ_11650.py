fa9cda3f2881281a9f737272329137ad195ad974
Sumin Kim <ksumin0730@naver.com>
2022-02-16 12:58:19 +0900

    sort

--- BOJ_11650.py
+++ BOJ_11650.py
@@ -1,3 +1,12 @@
 #좌표 정렬하기
 #sort
 
+import sys
+n = int(input())
+maps = []
+input = sys.stdin.readline
+for _ in range(n):
+    maps.append(list(map(int, input().split())))
+
+maps.sort()
+print(" ".join(map(str, maps)))
