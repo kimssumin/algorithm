2288cde64a61b806a76249524b1441bdb888fc61
Sumin Kim <ksumin0730@naver.com>
2022-02-20 21:26:37 +0900

    sort

--- BOJ_11652.py
+++ BOJ_11652.py
@@ -2,3 +2,14 @@
 #sort
 
 import sys
+from collections import Counter 
+
+n = int(input())
+input = sys.stdin.readline
+cards = []
+
+for _ in range(n):
+    cards.append(int(input()))
+
+count = Counter(cards)
+print(sorted(count.items(), key = lambda x: (-x[1], x[0]))[0][0])
