0ac7b00189c681fdf4cf0c94e423f5c040770757
Sumin Kim <ksumin0730@naver.com>
2022-02-23 16:56:14 +0900

    binary search

--- BOJ_2343.py
+++ BOJ_2343.py
@@ -1,3 +1,35 @@
 #기타 레슨
 #Binary Search
 
+import sys
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+tot = sum(arr)
+left, right = tot // m, sum(arr)
+
+ans = right
+
+while left <= right:
+    mid = (left + right) // 2
+    if mid < max(arr):
+        left = mid + 1
+        continue
+    count, temp = 0, 0
+    for i in range(len(arr)):
+        if arr[i] > mid:
+            break
+        elif temp + arr[i] <= mid:
+            temp += arr[i]
+        else:
+            temp = arr[i]
+            count += 1
+    if count <= m-1:
+        right = mid - 1
+        ans = min(ans, mid)
+    else:
+        left = mid + 1
+
+print(ans)
