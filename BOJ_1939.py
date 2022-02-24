6e410bc3f9a2def138881eee428802ba41d524c4
Sumin Kim <ksumin0730@naver.com>
2022-02-24 14:38:50 +0900

    binary search

--- BOJ_1939.py
+++ BOJ_1939.py
@@ -4,3 +4,38 @@
 import sys
 from collections import deque
 
+n, m = map(int, sys.stdin.readline().split())
+maps = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    y,x,w = map(int , sys.stdin.readline().split())
+    maps[y].append((x,w))
+    maps[x].append((y,w))
+
+start, end = map(int, sys.stdin.readline().split())
+mi, ma = 1, 100000000
+
+def bfs(c):
+    queue= deque()
+    queue.append(start)
+    visited = set()
+    visited.add(start)
+    result = []
+    while queue:
+        y = queue.popleft()
+        for x, w in maps[y]:
+            if x not in visited and w >= c:
+                visited.add(x)
+                queue.append(x)
+    return True if end in visited else False
+
+result = mi
+while mi <= ma:
+    mid = (mi+ma) //2
+    if bfs(mid):
+        result = mid
+        mi = mid + 1
+    else:
+        ma = mid - 1
+
+print(result)
