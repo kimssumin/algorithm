5b189355f8de5dbe2c9d2e922f33baed2241ce55
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-02-25 14:14:08 +0900

    binary search

---
+++ BOJ_1300.py
@@ -0,0 +1,22 @@
+#K번째 수
+# binary search
+
+import sys
+n = int(input())
+k = int(input())
+
+start, end = 1, K
+
+while start <= end:
+    mid = (start + end) // 2
+    
+    temp = 0
+    for i in range(1, N+1):
+        temp += min(mid//i, N) #mid 이하의 i의 배수 or 최대 N
+    
+    if temp >= K: #이분탐색 실행
+        answer = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+print(answer)
