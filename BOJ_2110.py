68763a81416ee0391c0a914d365194a3e86fa959
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-02-27 17:17:50 +0900

    binary search

--- BOJ_2110.py
+++ BOJ_2110.py
@@ -1,3 +1,39 @@
 #공유기 설치
 #binary search
 
+from multiprocessing.connection import answer_challenge
+import sys
+input = sys.stdin.readline
+
+n , c = map(int, input().split())
+home = []
+
+for _ in range(n):
+    home.append(int(input()))
+    
+home.sort()
+
+def binary_search(home, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        current = home[0]
+        cnt = 1
+
+        for i in range(1, len(home)):
+            if home[i] >= current + mid:
+                cnt += 1
+                current = home[i]
+
+        if cnt >= c :
+            global answer
+            start = mid + 1
+            answer = mid
+        else:
+            end = mid - 1
+
+start = 1
+end = home[-1] - home[0]
+answer = 0
+
+binary_search(home, start, end)
+print(answer)
