7471f6d463576ed9bce7fa736fa062f1bb3ffe8e
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-03-01 17:23:14 +0900

    DP

--- BOJ_2579.py
+++ BOJ_2579.py
@@ -11,11 +11,21 @@ dp = []
 for _ in range(n):
     st.append(int(input()))
 
-dp.append(st[0])
-dp.append(max(st[0] + st[1], st[1]))
-dp.append(max(st[0] + st[2], st[1] + st[2]))
+if n == 1:
+    dp.append(st[0])
+elif n == 2:
+    dp.append(st[0])
+    dp.append(max(st[0] + st[1], st[1]))
+elif n == 3 :
+    dp.append(st[0])
+    dp.append(max(st[0] + st[1], st[1]))
+    dp.append(max(st[0] + st[2], st[1] + st[2]))
 
-for i in range(3, n):
-    dp.append(max(dp[i-2] + dp[i], dp[i-3] + dp[i] + dp[i-1]))
+if n >= 4:
+    dp.append(st[0])
+    dp.append(max(st[0] + st[1], st[1]))
+    dp.append(max(st[0] + st[2], st[1] + st[2]))
+    for i in range(3, n):
+        dp.append(max(dp[i-2] + st[i], dp[i-3] + st[i] +st[i-1]))
 
 print(dp.pop())
