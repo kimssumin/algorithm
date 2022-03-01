0233ce3eb66f9a63667c71c0ee3ca1b34143a3ae
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-03-01 13:25:53 +0900

    DP

--- BOJ_15989.py
+++ BOJ_15989.py
@@ -1,2 +1,21 @@
 #1,2,3 더하기 4
 #DP
+
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+dp = [1] * 10001 #1만 써서 합되는 경우
+
+for i in range(2, 10001):
+    dp[i] += dp[i - 2] #2가 추가되는 경우
+    
+for i in range(3, 10001):
+    dp[i] += dp[i - 3] #3이 추가되는 경우
+
+
+for _ in range(T):
+    n = int(input())
+    print(dp[n])
