321bf460b7eec1d4a0d928b7255620cc4b060b68
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-03-03 22:23:59 +0900

    sort

--- PGM_kthnum.py
+++ PGM_kthnum.py
@@ -1,2 +1,12 @@
 # K번째 수
 # sort
+
+def solution(array, commands):
+    answer = []
+    for a in commands:
+        i, j, k = a[0] - 1, a[1] - 1, a[2] - 1
+        new = array[i:j+1]
+        new.sort()
+        answer.append(new[k])
+
+    return answer
