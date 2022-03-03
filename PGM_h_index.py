561c641b97f5b174f74ff308acf1cb9d19d79518
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-03-03 22:48:18 +0900

    sort

---
+++ PGM_h_index.py
@@ -0,0 +1,9 @@
+# h-index
+# sort
+
+def solution(citations):
+    citations.sort()
+    for idx, citation in enumerate(citations):
+        if citation >= len(citations) - idx:
+            return len(citations) - idx
+    return 0
