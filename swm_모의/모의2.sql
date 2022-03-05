fe66c24cc107cb142462bb0e35b7c726f51eab86
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-03-05 16:06:07 +0900

    sql

--- swm_모의/모의2.sql
+++ swm_모의/모의2.sql
@@ -1,7 +1,2 @@
-CREATE TABLE IF NOT EXISTS `customer` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(64) NOT NULL,
-  `birthday` DATE,
-  `mileage` INT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+insert into departments(departments_id, )
+values( )
