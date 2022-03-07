7ee718011b7070b44449d1484268a0ef65288153
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-03-07 17:25:48 +0900

    dp

--- BOJ_13398.py
+++ BOJ_13398.py
@@ -1,11 +1,12 @@
-#연속합2
-#DP
+# 연속합2
+# DP
+
 import sys
 
 n = int(input())
 input = sys.stdin.readline
 
-l = list(map(int , input().split()))
+l = list(map(int, input().split()))
 
 dp = [0] * n
 dr = [0] * n
@@ -13,7 +14,7 @@ dp[0] = l[0]
 dr[n-1] = l[n-1]
 
 for i in range(1, len(dp)):
-    dp[i] = max(dp[i-1] + l[i] , l[i])
+    dp[i] = max(dp[i-1] + l[i], l[i])
 
 for j in range(n-2, -1, -1):
     dr[j] = max(dr[j+1] + l[j], l[j])
@@ -23,7 +24,7 @@ for k in range(n):
     if k == 0 or k == n-1:
         dp2[k] = l[k]
         continue
-    else :
+    else:
         dp2[k] = dp[k-1] + dr[k+1]
 
 res = max(max(dp), max(dp2))
