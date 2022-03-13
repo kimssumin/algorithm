401c2fb30b48089a985a62924071269d1215f5b2
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-03-13 13:32:45 +0900

    two pointer

--- SCS_03_02.py
+++ SCS_03_02.py
@@ -1,33 +1,33 @@
+# 작으면서 가까운 값 찾기
 
-#투포인터 알고리즘 이용
+# 투포인터 알고리즘 이용
 #시간복잡도 : O(N)
-#리스트 이용
+# 리스트 이용
 
 import sys
 
-n = int(input()) #n의 값 입력
+n = int(input())  # n의 값 입력
 input = sys.stdin.readline
-A = list(map(int, input().split())) #A 리스트 입력 
-B = [0] #젤 첫번째 값은 무조건 0 이므로 미리 할당하고 시작
+A = list(map(int, input().split()))  # A 리스트 입력
+B = [0]  # 젤 첫번째 값은 무조건 0 이므로 미리 할당하고 시작
 
-right = 1 #A의 두번쨰 index 부터 탐색 시작하므로 1 로 설정
-left = right - 1 #가장 인접한 값이므로 가장 가까운 값부터 탐색
+right = 1  # A의 두번쨰 index 부터 탐색 시작하므로 1 로 설정
+left = right - 1  # 가장 인접한 값이므로 가장 가까운 값부터 탐색
 
-#연산의 시간복잡도는 O(1)
+# 연산의 시간복잡도는 O(1)
 
-while left < right and right < n: #left 값은 right 값보다 항상 작으면서 right 가 리스트 끝까지 갈때
-	if A[left] < A[right]: #왼쪽에 있는 요소가 오른쪽보다 작다면
-		B.append(A[left]) #B에 그 값을 추가한다 (시간복잡도 O(1))
-		right += 1 #다음원소로 넘어가고
-		left = right - 1 #다음원소의 하나 작은 인덱스로 새로 할당해서 새로 탐색을 시작
-	else :#왼쪽에 있는 요소가 오른쪽보다 크거나 같다면
-		if left -1 >= 0: #left 값이 0 이하가 되지 않는 한에서
-			left -= 1 #하나씩 왼쪽으로 이동하면서 조건에 맞도록 탐색
-			
-		elif left == 0: #left 값이 리스트 시작값까지 갔을때에도 조건을 만족하지않을때
-			B.append(0) #0을 추가하고 (시간복잡도 O(1))
-			right += 1 #다음 원소로 넘어간다
-			left = right - 1  #마찬가지로 다음원소의 하나 작은 인덱스로 새로 할당해서 새로 탐색을 시작
-			
-print(" ".join(map(str, B))) #한줄에 빈칸을 띄어쓰기로 해 출력하도록함
-		
+while left < right and right < n:  # left 값은 right 값보다 항상 작으면서 right 가 리스트 끝까지 갈때
+    if A[left] < A[right]:  # 왼쪽에 있는 요소가 오른쪽보다 작다면
+        B.append(A[left])  # B에 그 값을 추가한다 (시간복잡도 O(1))
+        right += 1  # 다음원소로 넘어가고
+        left = right - 1  # 다음원소의 하나 작은 인덱스로 새로 할당해서 새로 탐색을 시작
+    else:  # 왼쪽에 있는 요소가 오른쪽보다 크거나 같다면
+        if left - 1 >= 0:  # left 값이 0 이하가 되지 않는 한에서
+            left -= 1  # 하나씩 왼쪽으로 이동하면서 조건에 맞도록 탐색
+
+        elif left == 0:  # left 값이 리스트 시작값까지 갔을때에도 조건을 만족하지않을때
+            B.append(0)  # 0을 추가하고 (시간복잡도 O(1))
+            right += 1  # 다음 원소로 넘어간다
+            left = right - 1  # 마찬가지로 다음원소의 하나 작은 인덱스로 새로 할당해서 새로 탐색을 시작
+
+print(" ".join(map(str, B)))  # 한줄에 빈칸을 띄어쓰기로 해 출력하도록함
