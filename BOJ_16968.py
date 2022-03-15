c13970d9d3134e3e92b89a80e52223123aa6f4bf
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-03-16 00:09:40 +0900

    brute force

--- BOJ_16968.py
+++ BOJ_16968.py
@@ -1,2 +1,21 @@
 # 차량 번호판
 # brute force
+import sys
+input = sys.stdin.readline
+
+s = input()
+ans = 1
+if s:
+    ans = 26 if s[0] == 'c' else 10
+    for i in range(1, len(s)):
+        if s[i] == 'c':
+            if s[i - 1] == 'c':
+                ans *= 25
+            else:
+                ans *= 26
+        else:
+            if s[i - 1] == 'd':
+                ans *= 9
+            else:
+                ans *= 10
+print(ans)
