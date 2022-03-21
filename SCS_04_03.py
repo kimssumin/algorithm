a6a0045844919485e5ad24ea6f2403dd9d7a53c7
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-03-21 21:23:23 +0900

    back tracing

--- SCS_04_03.py
+++ SCS_04_03.py
@@ -1,11 +1,50 @@
 # 두 여왕
 # 체스보드에 놓을 수 있는 방법의 수
+n = int(input())
+
+ans = 0
 
 
+def is_promising(x):
+    if x == 0:
+        return True
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x] - row[i]) == abs(x - i):
+            return False
+
+    return True
+
+
+def n_queens(x):
+    global ans
+    if x == 2*i + (a * 1):
+        # print(row)
+        ans += 1
+
+    else:
+        for j in range(n):
+            # [x, i]에 퀸을 놓겠다.
+            row[x] = j
+            if is_promising(x):
+                n_queens(x+i)  # 2)
+
+
+for a in range(0, n-1):
+    for i in range(1, n-a):
+        row = [100] * n
+        n_queens(a)
+# for i in range(2, n):
+# row = [100] * n
+# n_queens(2)
+
+
+print(ans)
+'''
 n = int(input())
 
 ans = 0
 row = [0] * n
+visited = [False for _ in range(n)]
 
 
 def is_promising(x):
@@ -17,22 +56,39 @@ def is_promising(x):
 
 
 def n_queens(x):
-    #cnt = 0
+    #nt = 0
     global ans
-    if x == 2:
+    if x == n:
         ans += 1
+
+    elif x >= 2 and x != n:
+        print(row)
+        ans += 1
+        row[x-1] = 0
+        for i in range(n):
+            if visited[i]:
+              continue
+            row[x] = i
+            if is_promising(x):
+                visited[i] = True
+                n_queens(0)
 
     else:
         for i in range(n):
             # [x, i]에 퀸을 놓겠다.
+            if visited[i]:
+              continue
             row[x] = i
             if is_promising(x):
+                visited[i] = True
                 n_queens(x+1)
+                visited[i] = False
+                #cnt += 1
 
 
 n_queens(0)
 print(ans)
-print(row)
+'''
 '''
 from collections import deque
 import sys
