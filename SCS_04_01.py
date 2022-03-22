46e2011602e2e6e22eeabc1d92193a7d4ada7c8c
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-03-22 12:58:03 +0900

    two point

--- SCS_04_01.py
+++ SCS_04_01.py
@@ -10,8 +10,37 @@ input = sys.stdin.readline
 for _ in range(n):  # 정수 입력을 받음
     num.append(int(input()))  # O(n)
 num.sort()
-for i in range(1, n-1):
+
+cnt = 0
+c_n = end
+c = num[c_n]
+
+
+def setting(i, start):
+    global a
+    global b
     b = num[i]
+    a = num[start]
+
+
+def checking(a, b, c):
+    if 2*b - a <= c <= 3*b - 2*a:
+        return True
+    else:
+        return False
+
+
+for i in range(n-2, -1, -1):
+    start = i-1
+    setting(i, start)
+
+    if checking:  # 조건을 만족하면
+        cnt += start + 1  # a가 더 작아지는 경우는 모두 통과
+    else:  # 조건을 만족하지 않으면
+        while checking == False:
+            start -= 1
+            setting(i, start)
+
     while start < i and start < end:
         a = num[start]
         li = 3*b - 2*a
