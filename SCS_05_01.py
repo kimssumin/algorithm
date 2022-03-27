c5738ceb5c397b77a08852b1e6c07a1bf60e9209
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-03-27 12:33:10 +0900

    search

--- SCS_05_01.py
+++ SCS_05_01.py
@@ -1,1 +1,25 @@
 # 리스트 값을 오름차순으로 정렬한 후 왼쪽방향으로 몇번 rotation 이동
+import sys
+
+A = list(map(int, input().split()))
+
+find1 = len(A) // 2
+for i in range(1, find1):
+    if A[i] < A[i-1]:
+        k = i
+        print(k)
+        break
+    else:
+        k = -1
+        pass
+
+if k == -1:
+    for j in range(find1, len(A)+1):
+        if A[i] < A[i-1]:
+            k = i
+            print(k)
+            break
+        else:
+            pass
+
+    print(0)
