d8be8a91b2aa56581d2e91a4b1c0f1b48538ee5a
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-03-29 12:15:36 +0900

    search

--- SCS_05_02.py
+++ SCS_05_02.py
@@ -1,3 +1,16 @@
 # 샛강 건너기
 # 폭 L, n 개의 돌, 시작위치 0 , 끝나는 위치 L
 # 인접한 돌 사이의 최소 거리 이상 점프
+
+l, n, k = map(int ,input().split())
+import sys
+input = sys.stdin.readline
+stone = list(map(int, input().split()))
+dis = [stone[0]]
+
+for x in range(1, len(stone)):
+	dis.append(stone[x] - stone[x-1])
+dis.append(l - stone[n-1])
+print(dis)
+#for _ in range(k):
+	
