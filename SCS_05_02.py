afd222fd9f70dd1a7790ae6a9e38f8ffb5e9c390
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-03-29 13:20:58 +0900

    search

--- SCS_05_02.py
+++ SCS_05_02.py
@@ -2,15 +2,34 @@
 # 폭 L, n 개의 돌, 시작위치 0 , 끝나는 위치 L
 # 인접한 돌 사이의 최소 거리 이상 점프
 
-l, n, k = map(int ,input().split())
 import sys
+l, n, k = map(int, input().split())
 input = sys.stdin.readline
 stone = list(map(int, input().split()))
 dis = [stone[0]]
 
 for x in range(1, len(stone)):
-	dis.append(stone[x] - stone[x-1])
+    dis.append(stone[x] - stone[x-1])
 dis.append(l - stone[n-1])
-print(dis)
-#for _ in range(k):
-	
+# print(dis)
+
+for kk in range(1, k+1):
+    min_dis = 1e9
+    for y in range(len(dis)):
+        if dis[y] < min_dis:
+            min_dis = dis[y]
+            min_ind = y
+
+    new_dis = [0] * (n+1-kk)
+    for a in range(n+1-kk):
+        if a < y-1:
+            new_dis[a] = dis[a]
+        elif a == y-1:
+            new_dis[a] = dis[a+1] + dis[a]
+        else:
+            new_dis[a] = dis[a+1]
+    dis = new_dis
+    print(dis)
+    result = min(dis)
+
+print(result)
