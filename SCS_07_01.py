f38a94258dc22c0ec7f8a1320bcbcad8be4f16af
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-04-10 22:54:50 +0900

    7-1

--- SCS_07_01.py
+++ SCS_07_01.py
@@ -1,3 +1,10 @@
 # 필사의 탈출
 
 # 남왕국으로 자객의 탈출 - 장벽 세개 통과
+
+u = int(input())  # 가장 위 구멍의 수
+uu = list(map(int, input().split))  # 가장 위 구멍의 x 좌표
+m = int(input())  # 중간 구멍의 수
+mm = list(map(int, input().split))  # 중간 구멍의 x 좌표
+l = int(input())  # 가장 아래 구멍의 수
+ll = list(map(int, input().split))  # 가장 아래 구멍의 x 좌표
