e3806d208623e93f5a021c31cb457a7744111d67
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-04-27 15:51:01 +0900

    fenwick

--- SCS_08_02.py
+++ SCS_08_02.py
@@ -12,7 +12,8 @@ input = sys.stdin.readline
 
 
 n, q = map(int, input().split())
-#cost = list(map(int, input().split()))
+cost_init = list(map(int, input().split()))
+cnts_init = []
 tree = [[] for _ in range(n+2)]
 for _ in range(n-1):
     p, c = map(int, input().split())
@@ -20,33 +21,94 @@ for _ in range(n-1):
 
 visited = [0] * (n+1)
 preorder = [1]
+cost = [cost_init[0]]
+
+
+def counts(x):
+    global cnt
+    for i in tree[x]:
+        cnt += 1
+        if tree[i]:
+            counts(i)
+    return cnt + 1
+
+
+for i in range(1, n+1):
+    cnt = 0
+    cnts_init.append(counts(i))
+
+
+cnts = [cnts_init[0]]
 
 
 def dfs(x, l):
     visited[x] = True
-    #level[x] = I
-    # print(x)
     for i in tree[x]:
         if not visited[i]:
             #parent[i] = x
             preorder.append(i)
+            cost.append(cost_init[i-1])  # cost 작성 (preorder 순으로)
+            cnts.append(cnts_init[i-1])  # 자식노드 갯수 count
             dfs(i, l+1)
 
 
-dfs(1, 0)
+dfs(1, 0)  # preorder list 작성
 print(preorder)
-# def dfs(idx):  # subtree 노드 갯수 찾는 함수
-#     global cnt
-
-#     # 순회를 할때마다 카운트
-#     cnt += 1
-#     # 자식노드를 순회
-#     for i in tree[idx]:
-#         dfs(i)
+print(cost)
+print(cnts)
 
 
-# cnt = 0
-# dfs(n)  # 결과값 호출
+def LSB(k):
+    return k & -k
+
+
+T_cost = []
+for a in range(len(cost)):
+    if a % 2 == 0:
+        T_cost.append(cost[a])
+    else:
+        cc = 0
+        for k in range(a, a - LSB(a+1), -1):
+            cc += cost[k]
+        T_cost.append(cc)
+print(T_cost)
+
+
+def prefix_sum(k):
+    s = 0
+    while k >= 1:
+        s += T_cost[k-1]
+        k = k - LSB(k)
+    return s
+
+
+# print(prefix_sum(7))  # cost[6]까지의 합
+
+
+def update(k, x):  # T_cost[k]를 x+T_cost[k]로 change
+    while k <= n:  # (O(logN))
+        T_cost[k] = T_cost[k] + x
+        k = k + LSB(k+1)  # O(1)
+
+
+for _ in range(q):  # 질의
+    do = str(input())
+    #v = int(v)
+    if do.startswith('sub'):  # subtree
+        v = int(do.split()[1])
+        if v == 1:
+            result_sub = prefix_sum(n)
+            print(result_sub)
+        else:
+            i = preorder.index(v)
+            result_sub = prefix_sum(i + cnts[i]) - prefix_sum(i)
+            print(result_sub)
+    else:
+        v = int(do.split()[1])
+        d = int(do.split()[2])
+        i = preorder.index(v)
+        update(i, d)
+        cost[i] = d + cost[i]  # 비용 업데이트
 
 
 # def update_fenwick_tree(tree, index, value):
@@ -76,17 +138,3 @@ print(preorder)
 #         cost[b-1] = c
 #     elif a == 2:
 #         print(query_fenwick_tree(tree, c) - query_fenwick_tree(tree, b-1))
-
-
-# for _ in range(q):
-#     do = str(input())
-#     v = int(v)
-#     if do.startswith('sub'):
-#         v = int(do.split()[1])
-#         query_fenwick_tree(tree, 1) - query_fenwick_tree()
-#     else:
-#         v = int(do.split()[1])
-#         d = int(do.split()[2])
-
-#         update_fenwick_tree(tree, v, d)
-#         cost[v] = d + cost[v]  # 비용 업데이트
