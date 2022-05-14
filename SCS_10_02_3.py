5d4fbd1207cfaeedbf8f7badca4097a77a8b860c
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-05-14 21:31:02 +0900

    answer sol

---
+++ SCS_10_02_3.py
@@ -0,0 +1,22 @@
+def solve(a, b, t):
+    dp = [0] * (t+1)
+
+    for i in range(1, t+1):
+        first, second = -1, -1
+        if i >= a:
+            first = dp[i-a]
+        if i >= b:
+            second = dp[i-b]
+        if first == -1 and second == -1:
+            dp[i] = -1
+        else:
+            dp[i] = max(first, second) + 1
+    if dp[t] >= 0:
+        print(dp[t])
+        return
+    i = t-1
+    c = dp[i]
+    while c == -1:
+        i -= 1
+        c = dp[i]
+    print(c, t-i)
