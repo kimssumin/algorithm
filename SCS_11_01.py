55cd8bf4ba462c4a856c8a4017519cd7d28f8c2f
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-05-21 00:09:28 +0900

    dp

--- SCS_11_01.py
+++ SCS_11_01.py
@@ -6,16 +6,15 @@
 n, k = map(int, input().split())
 ground = []
 dp = [[0] * n for _ in range(n)]
+sums = []
+stack = []
 for i in range(n):
     ground.append(list(map(int, input().split())))
-    if n >= 3:
-        dp[i][0] = ground[i][0]
-        dp[i][1] = ground[i][0] + ground[i][1]
-        dp[i][2] = ground[i][0] + ground[i][1] + ground[i][2]
+    dp[i][0] = ground[i][0]
 
 if k > (n // 3) * n:
     print(-1)
 else:
     for i in range(n):
-        for j in range(3, n):
-            dp[i][j] = max(dp[i][j-1] + )
+        for j in range(1, n):
+            dp[i][j] = dp[i][j-1] + ground[j]
--- SCS_11_02.py
+++ SCS_11_02.py
@@ -1,2 +1,6 @@
 # 선물고르기
 #
+
+n = int(input())
+value = list(map(int, input().split()))
+cost = list(map(int, input().split()))
