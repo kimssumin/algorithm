caf989337d2bf0a68dd5a4ea1bf9fe14efc8d3f1
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-05-24 14:38:47 +0900

    brute

--- SCS_11_01_2.py
+++ SCS_11_01_2.py
@@ -3,7 +3,8 @@
 # 이익많게
 # dp
 
-from collections import deque
+# O(n^2)의 시간복잡도
+
 
 ans_line = []
 ans = []
@@ -12,8 +13,32 @@ ground = []
 for i in range(n):
     ground.append(list(map(int, input().split())))
 
-if k > (n // 3) * n:
+if k > (n // 3) * n:  # 타일을 놓을 수 있는 최대 갯수보다 k 가 큰 경우 다 놓을 수 없으므로 -1 출력
     print(-1)
+else:
+    # O(n ^2)의 시간복잡도
+    for i in ground:
+        start = 0
+        ans_line.append(i[start] + i[start+1] + i[start+2])  # 초기 상태
+        for j in range(1, n-2):
+            start += 1
+            if ans_line[-1] >= (i[start] + i[start+1] + i[start+2]):
+                continue
+            else:
+                ans_line.pop()
+                # 그 행에서 가장 큰 3개를 넣음
+                ans_line.append((i[start] + i[start+1] + i[start+2]))
+        ans.append(ans_line[-1])  # 그 행들을 모으면 전체 타일에서 놓을 수 있는 최대의 수
+
+    ans.sort()  # O(nlogn)
+    end = len(ans)
+    answer = 0
+    for _ in range(k):  # O(k)
+        if end != 0:
+            end -= 1
+            answer += ans[end]  # k개의 수만큼 큰 순으로 내림차순으로 고르면 최대값
+    print(answer)
+
 
 # elif n >= 6: # 한행에 두개 들어가는..
 #     cnt = n // 3
@@ -38,26 +63,3 @@ if k > (n // 3) * n:
 #             end -= 1
 #             answer += ans[end]
 #     print(answer)
-
-
-else:
-    for i in ground:
-        start = 0
-        ans_line.append(i[start] + i[start+1] + i[start+2])
-        for j in range(1, n-2):
-            start += 1
-            if ans_line[-1] >= (i[start] + i[start+1] + i[start+2]):
-                continue
-            else:
-                ans_line.pop()
-                ans_line.append((i[start] + i[start+1] + i[start+2]))
-        ans.append(ans_line[-1])
-
-    ans.sort()
-    end = len(ans)
-    answer = 0
-    for _ in range(k):
-        if end != 0:  # 여기서 runtime error 가 없어진 걸 보면 end == 0인게 있다 -> 예외설정제대로 못했다
-            end -= 1
-            answer += ans[end]
-    print(answer)
