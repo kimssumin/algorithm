3726affb4881cf04e05361423a5f0ef493f5d8c1
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-06-03 15:48:12 +0900

    LCA

--- SCS_12_01.py
+++ SCS_12_01.py
@@ -1,2 +1,11 @@
 # 합성함수
+# 최소 공통 조상 유형 동일
 
+n = int(input())
+func = [0]
+for _ in range(n):
+    func.append(int(input()))
+
+q = int(input())
+for _ in range(q):
+    x, k = map(int, input().split())
