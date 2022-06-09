a6c3d21da1789e375a719b78b410128ad8434054
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-06-09 14:50:16 +0900

    dijkstra

--- SCS_12_02.py
+++ SCS_12_02.py
@@ -1,10 +1,8 @@
-# 최선과 차선
-
-# 2번째 차선책의 합
 # dijkstra algorithm
+#다익스트라 알고리즘의 시간복잡도 O(mlogn) (m : 간선의 개수, n : 노드의 개수)
 
 import sys
-import heapq
+import heapq #우선순위 큐 구현을 위하여
 
 n = int(input())
 m = int(input())
@@ -16,17 +14,17 @@ graph = [[]for _ in range(n+1)]
 heap = []
 
 
-def dij_2nd(start):
-    heapq.heappush(heap, [0, start])
+def dij(start):
+    heapq.heappush(heap, [0, start]) #시작노드부터 탐색하기위해
     dp[start][0] = 0
-    while heap:
-        a, b = heapq.heappop(heap)
-        for n_n, wei in graph[b]:
-            n_w = wei+a
-            if n_w < dp[n_n][1]:
-                dp[n_n][1] = n_w
-                dp[n_n].sort()
-                heapq.heappush(heap, [n_w, n_n])
+    while heap: #heap에 남아있는 노드가 없으면 끝
+        a, b = heapq.heappop(heap) #탐색할 노드와 거리를 가지고옴
+        for new_des, wei in graph[b]:
+            n_w = wei + a #해당 노드를 거쳐갈때의 거리
+            if n_w < dp[new_des][1]: #알고있는 거리보다 작으면 갱신
+                dp[new_des][1] = n_w
+                dp[new_des].sort()
+                heapq.heappush(heap, [n_w, new_des]) #다음 인접 거리를 계산하기 위해 삽입
 
 
 for _ in range(m):
@@ -35,17 +33,15 @@ for _ in range(m):
 
 sum = 0
 
-dij_2nd(0)
-# dij_1st(0)
-# print(dp)
+
+dij(0)
+#다익스트라 알고리즘의 시간복잡도 O(mlogn) (m : 간선의 개수, n : 노드의 개수)
 
 for i in range(1, n):
-    if dp[i][1] == inf:
+    if dp[i][1] == inf: # 경로가 존재하지 않는 경우
         #print(str(i)+"번째 : -1")
         continue
-
     else:
-        sum += (dp[i][1])
+        sum += (dp[i][1]) # 두번째로 작은 값들의 합
         #print(str(i)+"번째 :" + str(dp[i][1]))
-
 print(sum)
