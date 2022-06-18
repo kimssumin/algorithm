830c5ad67adc26303e71ea0b931f985f34543876
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-06-18 15:31:40 +0900

    test

--- SCS_test_1.py
+++ SCS_test_1.py
@@ -1,3 +1,11 @@
-from itertools import combinations as com
-from itertools import permutations as per
-print(list(per([1, 2, 3], 2)))
+''' 정답 코드
+n = int(input())
+A = [int(x) for x in input().split()]
+
+A.sort()
+m = float("Inf")
+for i in range(n):
+    if m > A[i] + A[2*n-i-1]:
+        m = A[i] + A[2*n-i-1]
+print(m)
+'''
