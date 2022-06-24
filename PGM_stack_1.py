be445a2b2a18f889950600821ae097c0cc0f2392
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-06-24 22:12:41 +0900

    stack

--- PGM_stack_1.py
+++ PGM_stack_1.py
@@ -1,1 +1,33 @@
 # 기능 개발
+
+def solution(progresses, speeds):
+    answer = []
+    daylist = []
+    for pro in range(len(progresses)):
+        if (100 - progresses[pro]) % speeds[pro] == 0:
+            day = (100 - progresses[pro]) // speeds[pro]
+            daylist.append(day)
+        else:
+            day = (100 - progresses[pro]) // speeds[pro] + 1
+            daylist.append(day)
+    ans = 1
+    print(daylist)
+    for i in range(1, len(daylist)):
+        maxx = daylist[0]
+        if daylist[i] <= daylist[i-1]:
+            ans += 1
+            if i == len(daylist) - 1:
+                answer.append(ans)
+        else:
+            answer.append(ans)
+            ans = 1
+            maxx = daylist[i]
+            if i == len(daylist) - 1:
+                answer.append(ans)
+
+    return answer
+
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+print(solution(a, b))
