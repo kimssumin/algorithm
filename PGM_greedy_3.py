6594ccba682f10f5b6ff0dc24fc7e862acf3093d
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-06-26 01:34:04 +0900

    greedy

--- PGM_greedy_3.py
+++ PGM_greedy_3.py
@@ -1,5 +1,16 @@
 # 큰 수 만들기
 
+def solution(number, k):
+    answer = []  # Stack
+
+    for num in number:
+        while k > 0 and answer and answer[-1] < num:
+            answer.pop()
+            k -= 1
+        answer.append(num)
+
+    return ''.join(answer[:len(answer) - k])
+
 
 '''
 sol2) combination -> 시간초과
