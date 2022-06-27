2c9cbfa0200bc89bb3cbf5f2317985b4762f6238
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-06-27 16:28:02 +0900

    deque

--- PGM_dfs_1.py
+++ PGM_dfs_1.py
@@ -19,3 +19,19 @@ def solution(numbers, target):
             if temp == target:
                 answer += 1
     return answer
+
+
+'''def solution(numbers, target):
+    n = len(numbers)
+    answer = 0
+    def dfs(idx, result):
+        if idx == n:
+            if result == target:
+                nonlocal answer
+                answer += 1
+            return
+        else:
+            dfs(idx+1, result+numbers[idx])
+            dfs(idx+1, result-numbers[idx])
+    dfs(0,0)
+    return answer'''
