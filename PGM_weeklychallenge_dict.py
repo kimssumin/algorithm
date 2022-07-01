a1c382491790a2177aac97d4dc217a22030ea057
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-07-01 19:48:58 +0900

    brute, product

---
+++ PGM_weeklychallenge_dict.py
@@ -0,0 +1,14 @@
+# 모음사전
+# 중복순열 문제
+
+from itertools import product
+
+
+def solution(word):
+    words = []
+    for i in range(1, 6):
+        for c in product(['A', 'E', 'I', 'O', 'U'], repeat=i):
+            words.append(''.join(list(c)))
+
+    words.sort()
+    return words.index(word) + 1
