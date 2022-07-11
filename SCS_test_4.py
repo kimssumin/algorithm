89ba84e76334de7b16d2816e224094146670c2ae
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-07-11 13:50:26 +0900

    test

--- SCS_test_4.py
+++ SCS_test_4.py
@@ -1,12 +1,13 @@
 
 #solve sol
+
 def solve(A, k, n): # k x n product-price table, k products and n days
     total = [[float('Inf') for _ in range(n)] for _ in range(1<<k)]
     # total[S][d] = d일까지 부분집합 S에 속한 물건을 조건에 맞게 사는 데 필요한 최소 금액
     for d in range(n):
         total[0][d] = 0
     for x in range(k):
-        total[1<<x][0] = A[x][0]
+        total[1<<x][0] = A[x][0]#bit
 
     for d in range(1, n):
         for s in range(1, 1<<k): # bit respresentation for any subset
