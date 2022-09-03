1709424260a314677c76ad9de97c2d82d25af10c
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-09-03 15:46:51 +0900

    bruteforce

--- BOJ_16938.py
+++ BOJ_16938.py
@@ -1,23 +1,37 @@
 # 캠프준비
 # bruteforce
+from itertools import combinations
 
 n, l, r, x = map(int, input().split())
 level = list(map(int, input().rstrip().split()))
-level.sort()
 
-end = 1
-cnt = 0
-sum = 0
 
-for start in range(0, n-1):
-    sum += level[start]
-    while (end < n) and (sum <= r):
-        print(start, end)
-        sum += level[end]
+ans = 0
+for i in range(2, n + 1):
+    for com in combinations(level, i):
+        if l <= sum(com) <= r:
+            if max(com) - min(com) >= x:
+                ans += 1
 
-        if (l <= sum <= r) and (level[end] - level[start] >= x):
-            cnt += 1
-            print(start, end, 'success')
-        end += 1
-        sum -= level[start]
-print(cnt)
+print(ans)
+
+
+# two pointer algorithm 사용 실패
+# level.sort()
+
+# end = 1
+# cnt = 0
+# sum = 0
+
+# for start in range(0, n-1):
+#     sum += level[start]
+#     while (end < n) and (sum <= r):
+#         print(start, end)
+#         sum += level[end]
+
+#         if (l <= sum <= r) and (level[end] - level[start] >= x):
+#             cnt += 1
+#             print(start, end, 'success')
+#         end += 1
+#         sum -= level[start]
+# print(cnt)
