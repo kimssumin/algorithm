b0767ac112391462b3a36649016161a1ac4f90e5
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-09-12 15:14:32 +0900

    simulation

--- CDT_01_02_01.py
+++ CDT_01_02_01.py
@@ -7,23 +7,16 @@ u = list(map(int, input().split()))
 d = list(map(int, input().split()))
 
 for _ in range(t):
-    temp = u[n - 1]
 
-    # Step 2
-    # 위에 있는 숫자들을 완성합니다.
-    # 오른쪽에서부터 채워넣어야 하며,
-    # 맨 왼쪽 숫자는 아래에서 가져와야함에 유의합니다.
+    # 주어진 배열을 오른쪽으로 한칸 shift
+    temp = u[n - 1]
     for i in range(n - 1, 0, -1):
         u[i] = u[i - 1]
     u[0] = d[n - 1]
 
-    # Step 3
-    # 아래에 있는 숫자들을 완성합니다.
-    # 마찬가지로 오른쪽에서부터 채워넣어야 하며,
-    # 맨 왼쪽 숫자는 위에서 미리 저장해놨던 temp값을 가져와야함에 유의합니다.
     for i in range(n - 1, 0, -1):
         d[i] = d[i - 1]
-    d[0] = temp
+    d[0] = temp  # temp를 1번째 원소에 기록
 
 # 출력
 for elem in u:
