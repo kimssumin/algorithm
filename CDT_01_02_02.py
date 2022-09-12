3aff2f87dc3f2560e5223886c1ceb2f61df57908
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-09-12 17:02:25 +0900

    simulation-2

--- CDT_01_02_02.py
+++ CDT_01_02_02.py
@@ -5,6 +5,7 @@
 n, t = tuple(map(int, input().split()))
 u = list(map(int, input().split()))
 d = list(map(int, input().split()))
+r = list(map(int, input().split()))
 
 for _ in range(t):
 
@@ -12,11 +13,16 @@ for _ in range(t):
     temp = u[n - 1]
     for i in range(n - 1, 0, -1):
         u[i] = u[i - 1]
-    u[0] = d[n - 1]
+    u[0] = r[n - 1]
 
+    temp2 = d[n - 1]
     for i in range(n - 1, 0, -1):
         d[i] = d[i - 1]
     d[0] = temp  # temp를 1번째 원소에 기록
+
+    for i in range(n - 1, 0, -1):
+        r[i] = r[i - 1]
+    r[0] = temp2  # temp를 1번째 원소에 기록
 
 # 출력
 for elem in u:
@@ -24,4 +30,9 @@ for elem in u:
 print()
 
 for elem in d:
+    print(elem, end=" ")
+
+print()
+
+for elem in r:
     print(elem, end=" ")
