584b38e2a4df09c039d0dfbf168cffb2d559a1fe
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-09-14 17:34:32 +0900

    simulation-2

--- CDT_01_02_03.py
+++ CDT_01_02_03.py
@@ -5,24 +5,62 @@ wind = []
 for _ in range(n):
     city.append(list(map(int, input().split())))
 
+
+def move(row, dir):
+    if (dir == 'R'):
+        temp = city[row][m-1]
+        for i in range(m - 1, 0, -1):
+            city[row][i] = city[row][i - 1]
+        city[row][0] = temp
+
+    elif (dir == 'L'):
+        temp = city[row][0]
+        for i in range(0, m-1):
+            city[row][i] = city[row][i + 1]
+        city[row][m-1] = temp
+
+
+def check(row1, row2):
+    return any([
+        city[row1][col] == city[row2][col]
+        for col in range(0, m)
+    ])
+
+
+def flip(curr_dir):
+    return 'R' if curr_dir == 'L' else 'L'
+
+
+def simulate(start_row, start_dir):
+
+    move(start_row-1, start_dir)
+    start_dir = flip(start_dir)
+
+    curr_dir = start_dir
+    for row in range(start_row-1, 0, -1):
+        if check(row, row - 1):
+            move(row - 1, curr_dir)
+            curr_dir = flip(curr_dir)
+        else:
+            break
+
+    # 아래방향
+    curr_dir = start_dir
+    for row in range(start_row-1, n-1):
+        if check(row, row + 1):
+            move(row + 1, curr_dir)
+            curr_dir = flip(curr_dir)
+        else:
+            break
+
+
 for _ in range(q):
-    r, d = map(int, input().split())
+    r, d = map(str, input().split())
+    r = int(r)
     #wind = [(r, d), (r2, d2)]
-    if (d == 'L'):
-      # r-1 : 행
+    simulate(r, 'R' if d == 'L' else 'L')
 
-    else:
-      # R
-
-
-def check(row, d):
-    if row == d:
-        # 양방향 모두 검사
-        for i in range(len(city[row])):
-            if (city[row][i] == city[row-1][i]) or (city[row][i] == city[row+1][i]):
-                return True
-    elif row > d:
-        # 아래 방향 검사
-    else:
-        # 위방향 검사
-        for i in range(len(city[row])):
+for row in range(0, n):
+    for col in range(0, m):
+        print(city[row][col], end=" ")
+    print()
