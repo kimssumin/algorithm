9f6de55718924393e9de42fadbb45ef1b844ae7f
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-09-20 16:25:17 +0900

    simulation-done

--- CDT_01_04_01.py
+++ CDT_01_04_01.py
@@ -7,8 +7,8 @@ pann = []
 for _ in range(n):
     pann.append(list(map(int, input().split())))
 
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 base_x = r-1
 base_y = c-1
 answer = []
@@ -17,8 +17,9 @@ answer.append(pann[base_x][base_y])
 while(1):
     lists = []
     base = pann[base_x][base_y]
+    #print(base)
     for i in range(4):
-        if base_x+dx[i] > n or base_y+dy[i] > n or base_x+dx[i] < 0 or base_y+dy[i] < 0:
+        if base_x+dx[i] >= n or base_y+dy[i] >= n or base_x+dx[i] < 0 or base_y+dy[i] < 0:
             continue
         else:
             if pann[base_x+dx[i]][base_y+dy[i]] > base:
@@ -27,7 +28,8 @@ while(1):
     if len(lists) == 0:
         break
     else:
-        print('hi')
+        # print('hi')
+        # print(lists)
         base_x = lists[0][0]
         base_y = lists[0][1]
         answer.append(pann[base_x][base_y])
