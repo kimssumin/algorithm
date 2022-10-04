ecb35c0da55c14c5ff97700d0fc9ced7e88f295c
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-10-04 16:27:00 +0900

    simulation

--- CDT_01_04_02.py
+++ CDT_01_04_02.py
@@ -5,3 +5,30 @@ n, m, k = map(int, input().split())
 pann = []
 for _ in range(n):
     pann.append(list(map(int, input().split())))
+
+
+def all_blank(row, col_s, col_e):
+    return all([
+        not pann[row][col]
+        for col in range(col_s, col_e + 1)
+    ])
+
+
+def get_target_row():
+    for row in range(n - 1):
+        if not all_blank(row + 1, k, k + m - 1):
+            return row
+
+    return n - 1
+
+
+k -= 1
+
+target_row = get_target_row()
+
+for col in range(k, k + m):
+    pann[target_row][col] = 1
+
+
+for i in pann:
+    print(*i)
