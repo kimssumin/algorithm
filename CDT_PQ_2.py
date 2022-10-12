4c798fff59ccd1a383b5f52cdd7506310e39efb3
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-10-12 21:03:54 +0900

    priority queue

---
+++ CDT_PQ_2.py
@@ -0,0 +1,14 @@
+import heapq
+
+n = int(input())
+pq = []
+
+for _ in range(n):
+    x = int(input())
+    if x == 0:
+        if len(pq) == 0:
+            print(0)
+        else:
+            print(-heapq.heappop(pq))
+    else:
+        heapq.heappush(pq, -x)
