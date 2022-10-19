d27a191ba982da61cb2714d4261ba1c35f80a8d7
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-10-19 12:52:01 +0900

    for

---
+++ PGM_num.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = 0
+    for i in range(1, n+1):
+        s = 0
+        for j in range(i, n+1):
+            s += j
+            if s == n:
+                answer += 1
+                break
+            elif s > n:
+                break
+
+    return answer
+
+
+# /*2nd*/
