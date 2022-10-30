d5333dc542d1094e2b07d3f52979c28f7fc6e03d
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-10-30 14:25:59 +0900

    greedy

--- SWE_15230.py
+++ SWE_15230.py
@@ -1,1 +1,12 @@
 # 알파벳 공부
+answer = [chr(i) for i in range(97,123)]
+T = int(input())
+for test_case in range(1, T + 1):
+    inputs = list(input())
+    cnt = 0
+    for i,alpha in enumerate(inputs):
+        if alpha != answer[i]:
+            break
+        else:
+            cnt += 1
+    print('#'+str(test_case)+' ' +str(cnt))
