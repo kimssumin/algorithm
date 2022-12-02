c55672cb43c704bc36f86f633d6f66b89a026004
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-12-02 20:39:40 +0900

    bruteforce

--- PGM_2022kakao_dart.py
+++ PGM_2022kakao_dart.py
@@ -1,31 +1,62 @@
+from itertools import combinations as com
+
+
 def calcScore(arr):
     score = 0
-    for s ,idx in enumerate(arr):
-            if s != 0:
+    for idx, s in enumerate(arr):
+        if s != 0:
+            if 10-idx <= 0:
+                score += 0
+            else:
                 score += (10-idx)
     return score
 
+
+def ryanSuccess(peach, ryan):
+    new = [ai - bi for ai, bi in zip(peach, ryan)]
+    for idx, i in enumerate(new):
+        if i < 0:
+            new[idx] = 0
+    if (calcScore(new) < calcScore(ryan)):
+        return True
+    return False
+
+
 def solution(n, info):
-    score = [10-i for i in range(10)]
+    score = sum([10-i for i in range(10)])
+    peachscore = calcScore(info)
     ryanscore = 0
-    save_answer = []
-    check = False
-    for i in range(10):
-        peachscore = calcScore(info)
-        cnt = n
-        answer = []
-        answer.extend([0 for _ in range(i)])
-        for j in range(i, 10):
-            if cnt > 0 and info[j] + 1 >= n:
-                peachscore -= score[j]
-                answer.append(info[j] + 1)
-                cnt -= info[j] + 1
-        if peachscore < calcScore(answer):
-            if (ryanscore < calcScore(answer)):
-                ryanscore = calcScore(answer)
-                save_answer = answer
-                check = True
-                
-    if check == False:
+    basescore = [i+1 for i in info]
+    save = []
+    for i in range(1, 12):
+        for l in (list(com([x for x in range(11)], i))):
+            check_sumn = sum([basescore[b] for b in list(l)])
+            if check_sumn == n:
+                candidate = []
+                for i in range(11):
+                    if i in list(l):
+                        candidate.append(basescore[i])
+                    else:
+                        candidate.append(0)
+                print(candidate, calcScore(candidate))
+                if ryanscore < calcScore(candidate) and ryanSuccess(info, candidate):
+                    ryanscore = calcScore(candidate)
+                    save = candidate
+                elif ryanscore == calcScore(candidate) and ryanSuccess(info, candidate):
+                    for i in range(10, -1, -1):
+                        if save[i] == candidate[i]:
+                            continue
+                        elif save[i] < candidate[i]:
+                            save = candidate
+                            ryanscore = calcScore(candidate)
+                        else:
+                            break
+    if ryanscore == 0:
         return [-1]
-    return save_answer
+    else:
+        return [save, ryanscore]
+
+
+# 3,2,2,2,1,1,1,1,1,1,1
+#(solution(9, [0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1]))
+print(solution(10, [0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3]))
