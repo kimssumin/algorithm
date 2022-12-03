05cbf1917247e9562e8b503a0941d841b76136a2
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-12-03 11:27:44 +0900

    bruteforce

--- PGM_2022kakao_dart.py
+++ PGM_2022kakao_dart.py
@@ -1,4 +1,4 @@
-from itertools import combinations as com
+from itertools import combinations_with_replacement
 
 
 def calcScore(arr):
@@ -23,40 +23,32 @@ def ryanSuccess(peach, ryan):
 
 
 def solution(n, info):
-    score = sum([10-i for i in range(10)])
-    peachscore = calcScore(info)
-    ryanscore = 0
-    basescore = [i+1 for i in info]
-    save = []
-    for i in range(1, 12):
-        for l in (list(com([x for x in range(11)], i))):
-            check_sumn = sum([basescore[b] for b in list(l)])
-            if check_sumn == n:
-                candidate = []
-                for i in range(11):
-                    if i in list(l):
-                        candidate.append(basescore[i])
-                    else:
-                        candidate.append(0)
-                print(candidate, calcScore(candidate))
-                if ryanscore < calcScore(candidate) and ryanSuccess(info, candidate):
-                    ryanscore = calcScore(candidate)
-                    save = candidate
-                elif ryanscore == calcScore(candidate) and ryanSuccess(info, candidate):
-                    for i in range(10, -1, -1):
-                        if save[i] == candidate[i]:
-                            continue
-                        elif save[i] < candidate[i]:
-                            save = candidate
-                            ryanscore = calcScore(candidate)
-                        else:
-                            break
-    if ryanscore == 0:
-        return [-1]
-    else:
-        return [save, ryanscore]
+    answer = [-1]
+    max_gap = -1
+    # 중복 조합으로 0~10점까지 n개 뽑기
+    for combi in combinations_with_replacement(range(11), n):
+        candidate = [0] * 11  # 라이언의 과녁 점수
+
+        for i in combi:  # combi에 해당하는 화살들을 라이언 과녁 점수에 넣기
+            candidate[10 - i] += 1
+
+        apeach, ryon = 0, 0
+        for idx in range(11):
+            if info[idx] == candidate[idx] == 0:
+                continue
+            elif info[idx] >= candidate[idx]:
+                apeach += 10-idx
+            else:
+                ryon += 10 - idx
+
+        if ryon > apeach:
+            gap = ryon - apeach
+            if gap > max_gap:
+                max_gap = gap
+                answer = candidate
+    return answer
 
 
 # 3,2,2,2,1,1,1,1,1,1,1
-#(solution(9, [0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1]))
+# (solution(9, [0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1]))
 print(solution(10, [0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3]))
