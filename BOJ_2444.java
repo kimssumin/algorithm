be598c1df7525c50442334c20e2e8999a53e3dbf
Sumin Kim <ksumin0730@hufs.ac.kr>
2023-01-13 15:01:32 +0900

    --amend

---
+++ BOJ_2444.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class BOJ_2444 {
+	static int N;
+	public static void main(String[] args) {
+		Scanner scnn = new Scanner(System.in);
+		N = scnn.nextInt();
+		String star = "*";
+		
+		// 문자열 생성 : StringBuilder -> 출력이 많으면 시간이 많이 걸리므로 시간단축을 위해 문자열을 합침
+		StringBuilder sb = new StringBuilder(); // mutterable string
+		for (int i =0 ; i < N-1; i++) {
+			int cnt = 2*(i+1)-1;
+			int space_cnt = Math.abs((2*N - 1 - cnt) / 2);
+			for(int k =0 ; k < space_cnt ; k++) {
+				sb.append(" "); //non-static -> 개체를 생성해야함
+				//static 붙은 것은 객체 생성 안하는 것이 좋다.
+			}
+			for (int j = 0; j < cnt; j++) {
+				sb.append(star);
+			}
+			sb.append('\n');
+		}
+		for (int i =0 ; i < N; i++) {
+			int cnt = 2*(N-i)-1;
+			int space_cnt =Math.abs((2*N - 1 - cnt) / 2);
+			for(int k =0 ; k < space_cnt ; k++) {
+				sb.append(" ");
+			}
+			for (int j = 0; j < cnt; j++) {
+				sb.append(star);
+			}
+			sb.append('\n');
+		}
+		
+		
+		System.out.print(sb.toString()); // immuterable
+	}
+}
+
+
+// if-else문 이용해서도 가능
+
